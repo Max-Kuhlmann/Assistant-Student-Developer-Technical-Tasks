0d99b3b5a389d2ad626d49a9cae50d073e0e4234
Max <max.kuhlmann@ku.edu>
2026-04-22 23:57:30 -0500

    Make name into variable for reuse

--- group_by_category.py
+++ group_by_category.py
@@ -2,10 +2,11 @@ def group_by_category(items: list[dict]) -> dict:
     return_dict = {}
     
     for entry in items:
+        name = entry["name"]
         category = entry["category"]
         if category in return_dict.keys():
-            return_dict[category].append(entry["name"])
+            return_dict[category].append(name)
         else:
-            return_dict[category] = [entry["name"]]
+            return_dict[category] = [name]
     
     return return_dict
