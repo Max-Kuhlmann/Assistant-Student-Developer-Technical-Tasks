83264da93c290303acee61ac202abf3c0e755869
Max <max.kuhlmann@ku.edu>
2026-04-23 00:03:06 -0500

    Add comments for clarity

--- group_by_category.py
+++ group_by_category.py
@@ -1,9 +1,13 @@
 def group_by_category(items: list[dict]) -> dict:
+    "Sorts list of dict objects by category key."
     return_dict = {}
     
     for entry in items:
         name = entry["name"]
         category = entry["category"]
+        """If statement executes if category is already represented
+        and else executes if category is not yet in return_dict.
+        """
         if category in return_dict.keys():
             return_dict[category].append(name)
         else:
