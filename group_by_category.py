ee552a4888caa6dced9625824711e62dac8724f8
Max <max.kuhlmann@ku.edu>
2026-04-23 15:54:24 -0500

    Catch if dict is not properly formatted

--- group_by_category.py
+++ group_by_category.py
@@ -1,16 +1,24 @@
 def group_by_category(items: list[dict]) -> dict:
     "Sorts list of dict objects by category key."
     return_dict = {}
-    
+    incorrect_dicts = 0
+
     for entry in items:
-        name = entry["name"]
-        category = entry["category"]
-        """If statement executes if category is already represented
-        and else executes if category is not yet in return_dict.
-        """
-        if category in return_dict.keys():
-            return_dict[category].append(name)
-        else:
-            return_dict[category] = [name]
+        try:
+            name = entry["name"]
+            category = entry["category"]
+            """If statement executes if category is already represented
+            and else executes if category is not yet in return_dict.
+            """
+            if category in return_dict.keys():
+                return_dict[category].append(name)
+            else:
+                return_dict[category] = [name]
+        except KeyError or UnboundLocalError:
+            incorrect_dicts += 1
+        
+    
+    if incorrect_dicts != 0:
+        print(f"There was {incorrect_dicts} incorrectly formatted dict(s).")
     
     return return_dict
--- testing_file.py
+++ testing_file.py
@@ -1,12 +1,18 @@
 from group_by_category import group_by_category
 
 def main():
-    testing_list = [
+    testing_list_1 = [
                         {"name": "Apple", "category": "Fruit"},
                         {"name": "Carrot", "category": "Vegetable"},
                         {"name": "Banana", "category": "Fruit"}
                     ]
+    testing_list_2 = [
+                        {"eman": "Apple", "yrogetac": "Fruit"},
+                        {},
+                        {"name": "Banana", "category": "Fruit"}
+                    ]
 
-    print(group_by_category(testing_list))
+    print(group_by_category(testing_list_1))
+    print(group_by_category(testing_list_2))
 
 main()
